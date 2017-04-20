5b9a1acc1b40f9c76b2a30b49f44c55d174a6393
emily830518 <nrl@nrls-MacBook-Air.local>
2017-04-20 16:46:18 +0800

    add exception

--- main.go
+++ main.go
@@ -100,7 +100,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					}
 					// fmt.Println(airbox_json.Feeds[i].Device_id)
 				}
-				if txtmessage==nil{
+				if len(txtmessage)==0{
 					txtmessage="Sorry! No this device ID, please check again."
 				}
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(txtmessage)).Do(); err != nil {
