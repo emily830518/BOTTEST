8b914afff4dc292c235638621568d27f37983c19
emily830518 <nrl@nrls-MacBook-Air.local>
2017-04-20 16:28:39 +0800

    some revises

--- main.go
+++ main.go
@@ -94,8 +94,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						txtmessage="Device_id:"+airbox_json.Feeds[i].Device_id+"\n"
 						txtmessage=txtmessage+"Site Name:"+airbox_json.Feeds[i].SiteName+"\n"
 						txtmessage=txtmessage+"PM2.5:"+strconv.FormatFloat(float64(airbox_json.Feeds[i].S_d0),'f',0,64)+"\n"
-						txtmessage=txtmessage+"Humidity"+strconv.FormatFloat(float64(airbox_json.Feeds[i].S_h0),'f',0,64)+"\n"
-						txtmessage=txtmessage+"Temperature"+strconv.FormatFloat(float64(airbox_json.Feeds[i].S_t0),'f',0,64)
+						txtmessage=txtmessage+"Humidity:"+strconv.FormatFloat(float64(airbox_json.Feeds[i].S_h0),'f',0,64)+"\n"
+						txtmessage=txtmessage+"Temperature:"+strconv.FormatFloat(float64(airbox_json.Feeds[i].S_t0),'f',0,64)
 						break
 					}
 					// fmt.Println(airbox_json.Feeds[i].Device_id)
