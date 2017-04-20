72cdfac58fd006ce358a7998ff5dc324d11eb9f6
emily830518 <nrl@nrls-MacBook-Air.local>
2017-04-20 16:26:32 +0800

    reply message

--- main.go
+++ main.go
@@ -91,7 +91,12 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				inText := strings.ToLower(message.Text)
 				for i:=0; i<len(airbox_json.Feeds); i++ {
 					if inText==strings.ToLower(airbox_json.Feeds[i].Device_id) {
-						txtmessage="Device_id:"+airbox_json.Feeds[i].Device_id+"\n"+"PM2.5:"+strconv.FormatFloat(float64(airbox_json.Feeds[i].S_d0),'f',0,64)
+						txtmessage="Device_id:"+airbox_json.Feeds[i].Device_id+"\n"
+						txtmessage=txtmessage+"Site Name:"+airbox_json.Feeds[i].SiteName+"\n"
+						txtmessage=txtmessage+"PM2.5:"+strconv.FormatFloat(float64(airbox_json.Feeds[i].S_d0),'f',0,64)+"\n"
+						txtmessage=txtmessage+"Humidity"+strconv.FormatFloat(float64(airbox_json.Feeds[i].S_h0),'f',0,64)+"\n"
+						txtmessage=txtmessage+"Temperature"+strconv.FormatFloat(float64(airbox_json.Feeds[i].S_t0),'f',0,64)
+						break
 					}
 					// fmt.Println(airbox_json.Feeds[i].Device_id)
 				}
