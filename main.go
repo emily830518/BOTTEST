7db4446bf2eef5d88d87312c090255a99b5a3017
emily830518 <nrl@nrls-MacBook-Air.local>
2017-05-09 15:53:10 +0800

    test gps

--- main.go
+++ main.go
@@ -232,7 +232,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						if D<minD{
 							minD=D
 							txtmessage=strconv.FormatFloat(lat,'f',3,64)+","+strconv.FormatFloat(lon,'f',3,64)
-							txtmessage="Device_id: "+all_device[i].Device_id+"\n"
+							txtmessage=txtmessage+"Device_id: "+all_device[i].Device_id+"\n"
 							txtmessage=txtmessage+"Site Name: "+all_device[i].SiteName+"\n"
 							txtmessage=txtmessage+"Location: ("+strconv.FormatFloat(float64(all_device[i].Gps_lon),'f',3,64)+","+strconv.FormatFloat(float64(all_device[i].Gps_lat),'f',3,64)+")"+"\n"
 							txtmessage=txtmessage+"Timestamp: "+all_device[i].Timestamp+"\n"
